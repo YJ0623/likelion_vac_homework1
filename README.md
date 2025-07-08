026d5621a866b093f619b2c912006073bd924575
김준희 <shadd3338@gmail.com>
2025-07-08 20:11:38 +0900

    main페이지, 장바구니 구현

--- README.md
+++
@@ -1,5 +0,0 @@
-# likelion_vac_homework1
-멋사 방학세션 1주차 과제입니다.
-- develop 브랜치는 merge용 브랜치입니다
-- feature 브랜치는 develop 브랜치에 merge하기 위한 브랜치입니다.
-- feature 브랜치는 각 기능별로 분리되어 있고, main / product-detail / social-login / sighup / cart-page 총 5개로 제작할 예정입니다. 상황에 따라 추가할 수 있습니다.
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React,{useState} from "react";
+import SearchIcon from "../assets/SearchIcon.svg";
+
+
+const SearchBar=({onSearch})=>{
+  const [search,setSearch]=useState("");
+
+  const handleSearch =()=>{
+    if (search.trim()) onSearch(search);
+  }
+
+  return(
+    <div className="flex items-center w-full max-w-2xl">
+    <input
+      type="text"
+      placeholder="상품명을 검색하세요"
+      value={search}
+      onChange={e => setSearch(e.target.value)} 
+      onKeyDown={e => {
+        if (e.key === "Enter") handleSearch();
+      }}
+      className="border border-gray-300 rounded-l px-3 py-1 w-full focus:outline-none focus:border-[#4F46E5]"
+    />
+    <button
+      onClick={handleSearch}
+      className="bg-[#4F46E5] px-3 py-1 rounded-r flex items-center justify-center hover:bg-blue-600 cursor-pointer"
+      style={{ height: "36px" }}
+    >
+      <img src={SearchIcon} alt="검색아이콘" className="w-5 h-5" />
+    </button>
+  </div>
+  );
+};
+
+export default SearchBar;
+
