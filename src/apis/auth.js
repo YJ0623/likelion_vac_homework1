9fa2dc3eb7d60b7978af6782a34dce3716b3de5a
YJ <hghk0046@gmail.com>
2025-07-23 18:35:20 +0900

    fix: auth.js수정

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,7 +1,7 @@
 import axiosInstance from './axios';
 
 export const login = async (code) => {
-    const res = await axiosInstance.post('/auth/kakao', {
+    const res = await axiosInstance.post('login/oauth2/code/kakao', {
         code,
     });
     return res.data;
